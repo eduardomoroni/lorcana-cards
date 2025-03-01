90931a3d34f8c38c4bed9f04d06ee40350edb41c
Eduardo Morôni <eduardomoroni@gmail.com>
2025-03-01 09:49:38 +0100

    wip

---
+++ scripts/delete-avif.cjs
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const directory = path.join(__dirname, '../public');
+
+function deleteNonAvifFiles(dir) {
+    fs.readdir(dir, { withFileTypes: true }, (err, files) => {
+        if (err) throw err;
+
+        files.forEach(file => {
+            const filePath = path.join(dir, file.name);
+            if (file.isDirectory()) {
+                deleteNonAvifFiles(filePath);
+            } else if (path.extname(file.name) !== '.avif') {
+                fs.unlink(filePath, err => {
+                    if (err) throw err;
+                    console.log(`Deleted: ${filePath}`);
+                });
+            }
+        });
+    });
+}
+
+deleteNonAvifFiles(directory);
