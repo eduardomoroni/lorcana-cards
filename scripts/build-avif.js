da735d80326b5edebfc2ba546a8166e841bf2c31
Eduardo Morôni <eduardomoroni@gmail.com>
2025-03-01 09:44:52 +0100

    add scripts

---
+++ scripts/build-avif.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+const directory = path.join(__dirname, 'public');
+
+fs.readdir(directory, (err, files) => {
+    if (err) throw err;
+
+    files.forEach(file => {
+        if (path.extname(file) === '.webp') {
+            fs.unlink(path.join(directory, file), err => {
+                if (err) throw err;
+                console.log(`Deleted: ${file}`);
+            });
+        }
+    });
+});
