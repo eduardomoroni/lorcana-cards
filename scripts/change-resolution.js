9d0158d25218ab06d42f0ea856721bb320a1de05
Eduardo Morôni <eduardomoroni@gmail.com>
2025-03-06 07:13:34 +0100

    update bucky

--- scripts/change-resolution.js
+++ scripts/change-resolution.js
@@ -13,39 +13,54 @@ languages.forEach((language) => {
 
 async function program() {
 for (const sourceFolder of dirs) {
-  console.log("Reading files from " + sourceFolder);
   const files = fs.readdirSync(sourceFolder);
+  console.log("Reading files from " + sourceFolder + " total files: " + files.length);
 
   for (const file of files) {
     const dir = sourceFolder;
-console.log("Processing file " + file);
+    console.log("Processing file " + file);
+
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir);
     }
 
-    if (file.endsWith(".webp") && !file.endsWith("_2x.webp")) {
+    if (file.endsWith(".webp") || file.endsWith(".avif")) {
       const original = path.join(sourceFolder, file);
-      const copy = path.join(sourceFolder, file.replace(".webp", "_2x.webp"));
+      const copy = path.join(sourceFolder, file.replace(".webp", "_2x.webp").replace(".avif", "_2x.avif"));
 
       const image = await sharp(original);
       const sourceMetadata = await image.metadata();
 
-      if (sourceMetadata.width === 734 || sourceMetadata.height === 1024) {
-        console.log(`Image ${original} is already 734x1024. Skipping.`);
-        return;
+      const dimentions = {
+        width: 734,
+        height: 1024,
+      }
+
+      if (original.includes("/art_only/")) {
+        dimentions.width = 734;
+        dimentions.height = 603;
+      }
+
+      if (original.includes("/art_and_name/")) {
+        dimentions.width = 734;
+        dimentions.height = 767;
+      }
+
+      if (sourceMetadata.width === dimentions.width || sourceMetadata.height === dimentions.height) {
+        console.log(`Image ${original} is already ${dimentions.width}x${dimentions.height} Skipping.`);
+        continue;
       }
 
       if (!fs.existsSync(copy)) {
-        fs.copyFileSync(original, copy, (err) => {
-          if (err) {
-            console.error(`Error copying file ${original}:`, err);
-          } else {
-            console.log(`File ${original} copied successfully.`);
-          }
-        });
+        try {
+          fs.copyFileSync(original, copy);
+          console.log(`File ${original} copied successfully.`);
+        } catch (err) {
+          console.error(`Error copying file ${original}:`, err);
+        }
       }
       try {
-        await resizeImage(copy, original);
+        await resizeImage(copy, original, dimentions);
       } catch (err) {
         console.error(`Error resizing image ${copy}:`, err);
       }
@@ -65,11 +80,11 @@ console.log("Processing file " + file);
 
 // I'd like to use sharp JS to change image's resolution
 // taking half of height and half of width
-async function resizeImage(from, to) {
+async function resizeImage(from, to, dimentions) {
   console.log(`resizing ${from} ...`);
 
-  const resize = await image
-    .resize(Math.round(734), Math.round(1024))
+  const resize = await sharp(from)
+    .resize(dimentions.width, dimentions.height)
     .toFile(to)
     .catch(function (err) {
       console.log("Error resizing image: ", from);
